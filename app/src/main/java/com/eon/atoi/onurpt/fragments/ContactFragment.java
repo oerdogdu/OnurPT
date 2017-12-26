cab156c186fcf1bda991af76bc74af848207c9f8
Atoi <overdrive1907@gmail.com>
2017-12-26 17:49:00 +0300

    Contact Fragment Enhancements

--- app/src/main/java/com/eon/atoi/onurpt/fragments/ContactFragment.java
+++ app/src/main/java/com/eon/atoi/onurpt/fragments/ContactFragment.java
@@ -1,10 +1,15 @@
 package com.eon.atoi.onurpt.fragments;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.eon.atoi.onurpt.R;
 
@@ -38,6 +43,49 @@ public class ContactFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.contact_fragment, container, false);
+        Button facebookButton, instagramButton;
+
+        facebookButton = (Button)view.findViewById(R.id.facebookButton);
+        instagramButton = (Button)view.findViewById(R.id.instagramButton);
+
+        facebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent facebookIntent = getOpenFacebookIntent(getContext());
+                startActivity(facebookIntent);
+            }
+        });
+
+        instagramButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://instagram.com/_u/onurtrgt92");
+                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+                likeIng.setPackage("com.instagram.android");
+
+                try {
+                    startActivity(likeIng);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://instagram.com/onurtrgt92")));
+                }
+            }
+        });
+
         return view;
+    }
+
+    public static Intent getOpenFacebookIntent(Context context) {
+
+        try {
+            context.getPackageManager()
+                    .getPackageInfo("com.facebook.katana", 0);
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("fb://profile/759003957"));
+        } catch (Exception e) {
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.facebook.com/onur.turgut.714")); //catches and opens a url to the desired page
+        }
     }
 }
